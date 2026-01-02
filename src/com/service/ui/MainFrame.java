e724ed0863d42e97033ce2984b41391664c3631a
fburucu <75666945+fburucu@users.noreply.github.com>
2026-01-02 20:25:23 +0300

    Remove unused weekly rest packages and simplify structure

--- src/com/service/ui/MainFrame.java
+++ src/com/service/ui/MainFrame.java
@@ -3,6 +3,7 @@ package com.service.ui;
 import com.service.ui.tabs.StudentServiceTab;
 import com.service.ui.tabs.WeeklyRestTab;
 
+
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
